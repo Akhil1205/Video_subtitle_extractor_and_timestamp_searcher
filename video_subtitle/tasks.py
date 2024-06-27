72d910f9ab2c669f38b295b496ed7bb776532049
Akhileswar Reddy <chappidi.reddy@shadowfax.in>
2024-06-27 18:33:34 +0530

    final push

--- video_subtitle/tasks.py
+++ video_subtitle/tasks.py
@@ -8,7 +8,6 @@ from pynamodb.exceptions import PutError
 @shared_task
 def process_video(video_path, token):
     try:
-        import pdb; pdb.set_trace()
         s3_client = boto3.client('s3',aws_access_key_id=settings.AWS_ACCESS_KEY_ID,aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,region_name=settings.AWS_S3_REGION_NAME)        
         s3_client.upload_file(video_path, settings.AWS_STORAGE_BUCKET_NAME, os.path.basename(video_path))
         subprocess.run([settings.CCEXTRACTOR_PATH,video_path, '-o', 'output1.srt'])
--- video_subtitle/views.py
+++ video_subtitle/views.py
@@ -17,52 +17,56 @@ class VideoUploadView(APIView):
         return render(request, 'video_subtitle/upload.html')
     
     def post(self, request , *args, **kwargs):
-        token = request.headers.get('Authorization')
-        if not token or not Token.objects.filter(key=token).exists():
-            return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
-        print("getting video")
-        video = request.FILES.get('video')
-        print("got video")
-        VIDEO_NAME = video.name
-        video_path = os.path.join(settings.MEDIA_ROOT, VIDEO_NAME)
-        os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
-        
-        # Save the video file
-        with open(video_path, 'wb+') as destination:
-            for chunk in video.chunks():
-                destination.write(chunk)
-        existing_video_name =""
-        for i in TokenVideoMapping.query(token):
-            existing_video_name = i.video_name
-
-        if existing_video_name == VIDEO_NAME:
-            return Response({'message': 'Video already exists with this name'}, status=status.HTTP_200_OK)
-        elif existing_video_name=="":
-            token_video_mapping=TokenVideoMapping(user_token = token, video_name = VIDEO_NAME)
-            token_video_mapping.save()
-        else:
+        try:
+            token = request.headers.get('Authorization')
+            if not token or not Token.objects.filter(key=token).exists():
+                return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+            
+            video = request.FILES.get('video')
+            VIDEO_NAME = video.name
+            video_path = os.path.join(settings.MEDIA_ROOT, VIDEO_NAME)
+            os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
+            
+            # Save the video file
+            with open(video_path, 'wb+') as destination:
+                for chunk in video.chunks():
+                    destination.write(chunk)
+            existing_video_name =""
             for i in TokenVideoMapping.query(token):
-                i.video_name = VIDEO_NAME
-                i.save()
-        
-        process_video(video_path, token)
-        
-        return Response({'message': 'Video uploaded successfully'}, status=status.HTTP_200_OK)
+                existing_video_name = i.video_name
 
+            if existing_video_name == VIDEO_NAME:
+                return Response({'message': 'Video already exists with this name'}, status=status.HTTP_200_OK)
+            elif existing_video_name=="":
+                token_video_mapping=TokenVideoMapping(user_token = token, video_name = VIDEO_NAME)
+                token_video_mapping.save()
+            else:
+                for i in TokenVideoMapping.query(token):
+                    i.video_name = VIDEO_NAME
+                    i.save()
+            
+            process_video(video_path, token)
+            
+            return Response({'message': 'Video uploaded successfully'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 @method_decorator(csrf_exempt, name='dispatch')
 class SearchSubtitlesView(APIView):
     def get(self, request, *args, **kwargs):
         return render(request, 'video_subtitle/search.html')
     
     def put(self, request , *args, **kwargs):
-        token = request.headers.get('Authorization')
-        if not token or not Token.objects.filter(key=token).exists():
-            return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
-        
-        keyword = request.data.get('keyword')
-        results_list = get_data_from_db(keyword, token)
-        
-        return Response({'result': results_list}, status=status.HTTP_200_OK)
+        try:
+            token = request.headers.get('Authorization')
+            if not token or not Token.objects.filter(key=token).exists():
+                return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+            
+            keyword = request.data.get('keyword')
+            results_list = get_data_from_db(keyword, token)
+            
+            return Response({'result': results_list}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class LoginView(APIView):
