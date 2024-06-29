e2dfa521023fe6bd22cc82361c8ad856237d3a97
Akhileswar Reddy <chappidi.reddy@shadowfax.in>
2024-06-29 14:25:39 +0530

    final push

--- video_subtitle_fetcher/settings.py
+++ video_subtitle_fetcher/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0l_m8=_5#33w$=s4bu*)-+!ew8u=89o8!x6_ep6ju=-#3eh3$e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
